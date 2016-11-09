efa12355d150178edee8c1aae0c6ae67337f2e45
Stanley <stanleyyylau@gmail.com>
2016-11-09 09:56:47 +0800

    allow cross origin suport

--- server/server.js
+++ server/server.js
@@ -13,6 +13,12 @@ var {authenticate} = require('./middleware/authenticate');
 var app = express();
 const port = process.env.PORT;
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.use(bodyParser.json());
 
 app.post('/todos', authenticate, (req, res) => {
